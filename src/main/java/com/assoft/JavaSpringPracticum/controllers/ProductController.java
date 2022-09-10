984fa8be0349b41ff499ed770ce6edfb8f6248cd
Mehmet Hanifi Karaaslan <hanifi44karaaslan@gmail.com>
2022-09-10 16:21:53 +0300

    Creating controller layer.

---
+++ src/main/java/com/assoft/JavaSpringPracticum/controllers/ProductController.java
@@ -0,0 +1,10 @@
+package com.assoft.JavaSpringPracticum.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/product")
+public class ProductController {
+
+}
