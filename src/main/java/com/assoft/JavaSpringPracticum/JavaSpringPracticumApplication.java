e4f46ef1b0d464128f74c182986ed878f6b85842
Mehmet Hanifi Karaaslan <hanifi44karaaslan@gmail.com>
2022-09-10 14:20:27 +0300

    Create spring project

---
+++ src/main/java/com/assoft/JavaSpringPracticum/JavaSpringPracticumApplication.java
@@ -0,0 +1,13 @@
+package com.assoft.JavaSpringPracticum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringPracticumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringPracticumApplication.class, args);
+	}
+
+}
